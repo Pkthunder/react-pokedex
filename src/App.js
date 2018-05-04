c4902cb027a3820cba6f34ed567184445e525747
Jared Perreault <jared_perreault@yahoo.com>
2018-05-04 01:44:43 -0400

    began structuring page like a pokedex

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
-            </header>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
                 <Pokemon/>
             </div>
         );
